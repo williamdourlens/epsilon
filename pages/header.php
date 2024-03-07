a4a8b632fb866a314801ff503ff16f79333a1c10
William Dourlens <william.dourlens@ecoles-epsi.net>
2024-03-07 09:48:19 +0100

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
     <header>
         <h1 class="font-bold text-3xl mt-4 mb-2">EPSILON</h1>
         <h2 class="mb-3">Plateforme de peer-learning<br>EPSI Lille</h2>
